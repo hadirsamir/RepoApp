70167b86134142fb0012e1cef61b5fce18388b2a
HadirAbdElReheem <hadirhamedmahmoud>
2020-12-12 21:43:39 +0200

    add search logic with searc bar and alertView

--- RepoApp/CustomUIViewsClasses/SpinnerIndicator&AlertView.swift
+++ RepoApp/CustomUIViewsClasses/SpinnerIndicator&AlertView.swift
@@ -34,3 +34,9 @@ class SpinnerIndicator{
         spinner?.stopAnimating()
     }
 }
+class AlertView: NSObject{
+    class func showAlert(view: UIViewController , title: String , message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+        view.present(alert, animated: true, completion: nil)
+    }
